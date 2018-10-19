544f6b33845a45e48922d2526341bf4eff2b9dac
Sharang Pai <sharangpai123@gmail.com>
2018-10-19 20:08:55 +0530

    added gitignore

---
+++ testScript.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+v1 = tf.Variable(0.0)
+p1 = tf.placeholder(tf.float32)
+new_val = tf.add(v1, p1)
+update = tf.assign(v1, new_val)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for _ in range(5):
+        sess.run(update, feed_dict={p1: 1.0})
+    print(sess.run(v1))
